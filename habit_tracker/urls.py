9acb72d6bd36445bc19180841699ebc9e39379b3
Julia Bengert <yubengert@gmail.com>
2025-03-18 18:42:22 +0300

    add public habit list

--- habit_tracker/urls.py
+++ habit_tracker/urls.py
@@ -8,6 +8,7 @@ app_name = HabitTrackerConfig.name
 urlpatterns = [
     path("habit_create/", views.HabitCreateAPIView.as_view(), name="habit_create"),
     path("habit_list/", views.HabitListAPIView.as_view(), name="habit_list"),
+    path("habit_list/public/", views.HabitPublicListAPIView.as_view(), name="habit_public_list"),
     path("habit_retrieve/<int:pk>/", views.HabitRetrieveAPIView.as_view(), name="habit_retrieve"),
     path("habit_update/<int:pk>/", views.HabitUpdateAPIView.as_view(), name="habit_update"),
     path("habit_delete/<int:pk>/", views.HabitDestroyAPIView.as_view(), name="habit_delete"),
--- habit_tracker/views.py
+++ habit_tracker/views.py
@@ -7,6 +7,7 @@ from .pagination import HabitListPageNumberPagination
 
 
 class HabitCreateAPIView(generics.CreateAPIView):
+
     serializer_class = HabitSerializer
 
     def perform_create(self, serializer):
@@ -14,24 +15,37 @@ class HabitCreateAPIView(generics.CreateAPIView):
 
 
 class HabitListAPIView(generics.ListAPIView):
-    queryset = Habit.objects.all()
+
+    serializer_class = HabitSerializer
+    pagination_class = HabitListPageNumberPagination
+
+    def get_queryset(self):
+        return Habit.objects.filter(user=self.request.user)
+
+
+class HabitPublicListAPIView(generics.ListAPIView):
+
+    queryset = Habit.objects.filter(is_public=True)
     serializer_class = HabitSerializer
     pagination_class = HabitListPageNumberPagination
 
 
 class HabitRetrieveAPIView(generics.RetrieveAPIView):
+
     queryset = Habit.objects.all()
     serializer_class = HabitSerializer
     permission_classes = [IsAuthenticated, IsOwnerHabit]
 
 
 class HabitUpdateAPIView(generics.UpdateAPIView):
+
     queryset = Habit.objects.all()
     serializer_class = HabitSerializer
     permission_classes = [IsAuthenticated, IsOwnerHabit]
 
 
 class HabitDestroyAPIView(generics.DestroyAPIView):
+
     queryset = Habit.objects.all()
     serializer_class = HabitSerializer
     permission_classes = [IsAuthenticated, IsOwnerHabit]
